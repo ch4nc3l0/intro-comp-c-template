85516829fcd8661f65fbf977f890e93c0c70b32f
chance <ch4nc3l0@gmail.com>
2019-09-01 10:47:50 -0400

    Init

---
+++ Template for School/source.c
@@ -0,0 +1,15 @@
+//**************************************************************
+//...Written by..: Chance Gurley
+//...Date Written: Aug 29, 2019
+//...Purpose.....: Programming class
+//**************************************************************
+#define _CRT_SERCURE_NO_WARNINGS // Allows use of scanf instead of scanf_s
+#include <stdio.h>
+#include <stdlib.h>
+#define PAUSE system("pause")
+#define CLS system("cls")
+
+main() {
+	//code here
+	PAUSE;
+} // end of main
