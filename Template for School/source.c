9db353730a77d7da00bed6433d95cd9d796ac2c9
chance <ch4nc3l0@gmail.com>
2019-09-15 20:46:44 -0400

    Feat: add attempt counter

--- Template for School/source.c
+++ Template for School/source.c
@@ -21,26 +21,26 @@ main() {
 	// State app purpose
 	printf("Guessing game can you guess a random number 1-100 with only 10 guesses?\n");
 	// Prompt user to input for first attempt
-	printf("Please make your first guess!/n");
+	printf("Please make your first guess!\n");
 	// Collect user input for first attempt
 	scanf(" %i", &userGuess);
 	// Start loop
-	for (int i = 0; i < 11; i++) {
+	for (int i = 0; i < 10; i++) {
 		// Check if user already made 10 guesses
-		if (i == 11) {
+		if (i == 9) {
 			printf("Unfortunalty you lost, the number was %i\n", randomAnswer);
 		}
 		// Check if user guessed lower
 		else if (userGuess < randomAnswer) {
 			// Print you guessed lower
-			printf("You guessed lower!\n");
+			printf("You guessed lower! You have %i attempts left.\n", 9 - i);
 			// Get user input
 			scanf(" %i", &userGuess);
 		}
 		// Check if user guessed higher
 		else if (userGuess > randomAnswer) {
 			// Print you guessed higher
-			printf("You guessed higher!\n");
+			printf("You guessed higher! You have %i attempts left.\n", 9 - i);
 			// Get user input
 			scanf(" %i", &userGuess);
 		}
