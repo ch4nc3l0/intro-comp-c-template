7c58ee0bc943c8d79559517fbb17b24fdde8868f
chance <ch4nc3l0@gmail.com>
2019-09-17 21:41:26 -0400

    number

--- Template for School/source.c
+++ Template for School/source.c
@@ -12,51 +12,53 @@
 
 main() {
 	// Set Variables
-	int randomAnswer;
-	int userGuess; 
-	// I want to challenge myself to not make a number of guesses variable
-	// Generate random answer
+	int randomNumbers[10];
+	int userInput;
+	int changeLeftover;
+	int half;
+	int quarter;
+	int dime;
+	int nickle;
+	int penney;
+	// Print app purpose and instructions
+
+	// Generate random numbers
 	srand(time(0)); // Generate random numbers every time program is run
-	randomAnswer = (rand() % (100 - 1 + 1)) + 1;
-	// State app purpose
-	printf("Guessing game can you guess a random number 1-100 with only 10 guesses?\n");
-	// Prompt user to input for first attempt
-	printf("Please make your first guess!\n");
-	// Collect user input for first attempt
-	scanf(" %i", &userGuess);
-	// Start loop
+	// Loop 10 times
 	for (int i = 0; i < 10; i++) {
-		// Check if user already made 10 guesses
-		if (i == 9) {
-			printf("Unfortunalty you lost, the number was %i\n", randomAnswer);
-		}
-		// Check if user guessed lower
-		else if (userGuess < randomAnswer) {
-			// Print you guessed lower
-			printf("You guessed lower! You have %i attempts left.\n", 9 - i);
-			// Get user input
-			scanf(" %i", &userGuess);
-		}
-		// Check if user guessed higher
-		else if (userGuess > randomAnswer) {
-			// Print you guessed higher
-			printf("You guessed higher! You have %i attempts left.\n", 9 - i);
-			// Get user input
-			scanf(" %i", &userGuess);
-		}
-		// Check if user guessed correct answer
-		else if (userGuess == randomAnswer) {
-			// Print you won
-			printf("Congrats! You won :)\n");
-			// Set i check to 12 to end loop
-			i = 12;
-		}
+		randomNumbers[i] = (rand() % (99 - 1 + 1)) + 1;
+		printf("New number %i\n", randomNumbers[i]);
 
-	}
+		while (randomNumbers[i] > 0) {
 
-
+			if (randomNumbers[i] >= 50) {
+				half = randomNumbers[i] / 50;
+				randomNumbers[i] =  randomNumbers[i] - half * 50;
+				printf("Halfs %i, ", half);
+			}
+			if (randomNumbers[i] >= 25) {
+				quarter = randomNumbers[i] / 25;
+				randomNumbers[i] =  randomNumbers[i] - quarter * 25;
+				printf("Quarters %i, ", quarter);
+			}
+			if (randomNumbers[i] >= 10) {
+				dime = randomNumbers[i] / 10;
+				randomNumbers[i] = randomNumbers[i] - dime * 10;
+				printf("Dimes %i, ", dime);
+			}
+			if (randomNumbers[i] >= 5) {
+				nickle = randomNumbers[i] / 5;
+				randomNumbers[i] = randomNumbers[i] - nickle * 5;
+				printf("Nickles %i, ", nickle);
+			}
+			if (randomNumbers[i] >= 1) {
+				penney = randomNumbers[i] / 1;
+				randomNumbers[i] = randomNumbers[i] - penney;
+				printf("Penneys %i\n", penney);
+			}
+		}
 		
-
+	}
 	
 
 	PAUSE;
