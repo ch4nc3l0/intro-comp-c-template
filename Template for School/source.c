5a71bb9cc74cd19eec892ba24cb8609f1553c709
chance <ch4nc3l0@gmail.com>
2019-09-22 21:09:23 -0400

    output changes

--- Template for School/source.c
+++ Template for School/source.c
@@ -21,13 +21,17 @@ main() {
 	int nickle;
 	int penney;
 	// Print app purpose and instructions
-
+	printf("This app will generate 10 random numbers between 1-99 and give change in the lowest amount of change\n");
+	printf("Press enter key to begin\n");
+	printf("---------------------------");
+	getchar();
 	// Generate random numbers
 	srand(time(0)); // Generate random numbers every time program is run
 	// Loop 10 times
 	for (int i = 0; i < 10; i++) {
 		randomNumbers[i] = (rand() % (99 - 1 + 1)) + 1;
-		printf("New number %i\n", randomNumbers[i]);
+		printf("Get change for %i cents:\n", randomNumbers[i]);
+		
 
 		while (randomNumbers[i] > 0) {
 
@@ -54,8 +58,11 @@ main() {
 			if (randomNumbers[i] >= 1) {
 				penney = randomNumbers[i] / 1;
 				randomNumbers[i] = randomNumbers[i] - penney;
-				printf("Penneys %i\n", penney);
+				printf("Penneys %i", penney);
 			}
+		}
+		if (randomNumbers[i] == 0) {
+			printf("\n---------------------------\n");
 		}
 		
 	}
