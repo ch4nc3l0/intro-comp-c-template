5c23a1a522c06ea04e9bea45b1ebbcd70e1f4b30
chance <ch4nc3l0@gmail.com>
2019-09-27 19:36:39 -0400

    init

--- Template for School/source.c
+++ Template for School/source.c
@@ -1,6 +1,6 @@
 //**************************************************************
 //...Written by..: Chance Gurley
-//...Date Written: Sep 13, 2019
+//...Date Written: SEPT 27, 2019
 //...Purpose.....: Programming class
 //**************************************************************
 #define _CRT_SECURE_NO_WARNINGS // Allows use of scanf instead of scanf_s
@@ -13,63 +13,7 @@
 
 main() {
 	// Set Variables
-	int inputIsNumber = 0;
-	int userInput;
-	int half;
-	int quarter;
-	int dime;
-	int nickle;
-	int penney;
-	// Print app purpose and instructions
-	printf("This app will generate lowest amount of change for a number in between 1-99\n");
-	printf("Please enter change amount 1-99\n");
-	printf("---------------------------\n");
-	// Grab user input
-	while (inputIsNumber == 0) {
-		scanf(" %i", &userInput);
-		getchar();
-		if (userInput > 99 || userInput < 1) {
-			printf("Please enter a number between 1-99\n");
-		}
-		else {
-			inputIsNumber = 1;
-		}
-		
-	}
 
-	do {
-		if (userInput >= 50) {
-			half = userInput / 50;
-			userInput = userInput - half * 50;
-			printf("Halfs %i, ", half);
-		}
-		if (userInput >= 25) {
-			quarter = userInput / 25;
-			userInput = userInput - quarter * 25;
-			printf("Quarters %i, ", quarter);
-		}
-		if (userInput >= 10) {
-			dime = userInput / 10;
-			userInput = userInput - dime * 10;
-			printf("Dimes %i, ", dime);
-		}
-		if (userInput >= 5) {
-			nickle = userInput / 5;
-			userInput = userInput - nickle * 5;
-			printf("Nickles %i, ", nickle);
-		}
-		if (userInput >= 1) {
-			penney = userInput / 1;
-			userInput = userInput - penney;
-			printf("Penneys %i", penney);
-		}
-	} while (userInput > 0);
-	printf("\n");
-
-
-		
-		
-	
 	
 
 	PAUSE;
