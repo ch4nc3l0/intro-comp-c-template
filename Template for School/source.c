69d5ccda9fa46ecc20da2b4d32f688b4b2f7c6ca
chance <ch4nc3l0@gmail.com>
2019-09-28 21:01:31 -0400

    Feat: Input validation

--- Template for School/source.c
+++ Template for School/source.c
@@ -15,13 +15,20 @@ main() {
 	// Set Variables
 	int userInput;
 
-
 	// Print progam instructions
-	printf("This app will output the shape of a triangle or diamond made of asterisks with the amount of rows equal to the input");
-
-
+	printf("This app will output the shape of a triangle or diamond made of asterisks with the amount of rows equal to the input\n");
 	// Get user input
-
+	printf("Input the number of asterisks 1-50\n");
+	scanf(" %i", &userInput);
+	// Just in case user enter a char program doesn't just loop forever || not perfect but works in this application decently
+	getchar();
+	// Verify user input
+	while ((userInput < 1) || (userInput > 50)) {
+		printf("Please enter a valid input\n");
+		scanf("%i", &userInput);
+		// Just in case user enter a char program doesn't just loop forever
+		getchar();
+	}
 	// Ask user how they want the print | triangle | Upside down | Backwards | Backwards & Upside-down | Diamond
 
 
