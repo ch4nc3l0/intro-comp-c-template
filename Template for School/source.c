48cb49b14d5c8051212544fc43892e60fef39399
chance <ch4nc3l0@gmail.com>
2019-10-02 15:21:06 -0400

    Feat: diamond printing

--- Template for School/source.c
+++ Template for School/source.c
@@ -16,6 +16,9 @@ main() {
 	// Set Variables
 	int userInput;
 	int counter;
+	int spaces;
+	int print;
+	int i = 0;
 	char asterisks[300] = "*";
 	char userString[50];
 
@@ -75,7 +78,91 @@ main() {
 		}
 	}
 
-	
+	// Loop to create diamond
+	// Check if input is diamond
+	if (!strcmp("diamond", userString)) {
+		while ((userInput < 10)||(userInput > 50))
+		{
+			printf("For a diamond shape please enter a digit greater than 10 but still less than 50\n");
+			scanf("%i", &userInput);
+		}
+
+		printf("Printing a diamond with the middle row being %i *s \n", userInput);
+
+
+			// Start loop for first half of diamond
+			for (counter = userInput; counter > 0; counter--) {
+				
+				// Keep track of how many iterations (I wanted to keep counter equal to userinput for spaces)
+				i++;
+
+				// Loop to insert spaces
+				for (spaces = 0; spaces < (counter / 2) ; spaces++) 
+					printf(" ");
+
+				// Loop to print *
+				for (print = i; print > 0;print--)
+				printf("*");
+
+			// Print newline after each iteration
+			printf("\n");
+			}
+			
+			// Start loop for second half of diamond
+			for (counter = 0; counter < userInput; counter++) {
+
+				// Loop to insert spaces
+				for (spaces = 0; spaces < (counter / 2); spaces++)
+					printf(" ");
+
+				// Loop to print *
+				for (print = 0; print < userInput - counter - 1; print++)
+					printf("*");
+
+				// Print newline after each iteration
+				printf("\n");
+			}
+	}
+
+	// Print upsideDown
+	if (!strcmp("upsideDown", userString)) {
+
+		// Loop equal to the num user entered
+		for (counter = userInput; counter > 0; counter--) {
+
+			// Print *
+			for (print = 0; print < counter; print++)
+				printf("*");
+
+			// print newline
+			printf("\n");
+
+			// Add a somewhat funny joke
+			if (userInput == 1) {
+				printf("Not much of a %s huh?\n", userString);
+			}
+		}
+	}
+
+	if (!strcmp("upsideDown", userString)) {
+
+		// Loop equal to the num user entered
+		for (counter = userInput; counter > 0; counter--) {
+
+			// Print *
+			for (print = 0; print < counter; print++)
+				printf("*");
+
+			// print newline
+			printf("\n");
+
+			// Add a somewhat funny joke
+			if (userInput == 1) {
+				printf("Not much of a %s huh?\n", userString);
+			}
+		}
+	}
+
 
 	PAUSE;
 } // end of main
