b224162d8d1817db7a3c5806ddf5eb925ca25c8a
chance <ch4nc3l0@gmail.com>
2019-10-22 19:25:09 -0400

    init

--- Template for School/source.c
+++ Template for School/source.c
@@ -1,7 +1,7 @@
 //**************************************************************
 //...Written by..: Chance Gurley
-//...Date Written: OCT 15, 2019
-//...Purpose.....: Programming class
+//...Date Written: OCT 22, 2019
+//...Purpose.....: Menu Assignment
 //**************************************************************
 #define _CRT_SECURE_NO_WARNINGS // Allows use of scanf instead of scanf_s
 #include <stdio.h>
@@ -13,104 +13,7 @@
 #define CLS system("cls")
 
 main() {
-	// Set Variables
-	int userInput;
-	int arr[5000];
-	int counter = 1;
-	int sumResult;
-	int largest = -50;
-	int lowest = 100;
-	int average;
-	int productResult;
-
-	// Print progam instructions
-	printf("This app will output facts about the numbers that are input to the program\n");
-
-	// Get user input for the first time
-	printf("Input a number from -50 to 100 or enter -999 to quit the application\n");
-	scanf(" %i", &userInput);
-
-	// While program is not at end condition run code
-	while (userInput != -999) {
-
-		// Verify user input
-		while ((userInput < -50) || (userInput > 100)) {
-			printf("Please enter a valid input\n");
-			scanf("%i", &userInput);
-		}
-		
-		// Add valid number to the array
-		arr[counter] = userInput;
-
-		// Print sum of numbers
-		sumResult = 0; // Reset result each time a new input is recieved
-		for (int i = counter; i>0;  i--) {
-			sumResult += arr[i];
-		}
-		printf("The sum of the numbers is %i\n", sumResult);
-
-		// Print Product of numbers
-		productResult = 1;
-		for (int i = counter; i > 0; i--) {
-			productResult *= arr[i];
-		}
-		printf("The product of the numbers is %i\n", productResult);
-
-		// Print the largest number
-		if (largest < userInput) {
-			largest = userInput;
-		}
-		printf("The highest of the numbers is %i\n", largest);
-
-		// Print the lowest number
-		if (lowest > userInput) {
-			lowest = userInput;
-		}
-		printf("The lowest of the numbers is %i\n", lowest);
-
-		// Print the average of the numbers
-		average = sumResult / counter;
-		printf("The average of the numbers is %i\n", average);
-
-		// Print how many numbers were input
-		printf("You have entered %i numbers\n", counter);
-
-		// Print if number is even or odd
-		if (userInput % 2 == 0) {
-			printf("The number %i is an even number\n", userInput);
-		}
-		else {
-			printf("The number %i is an odd number\n", userInput);
-		}
-		
-		// Print if prime number
-		int isPrime = 1;
-		for (int i = 2; i < userInput; i++) {
-			if ((userInput % i) == 0) {
-				isPrime = 0;
-			}
-		}
-		// Check if negitive => not a prime number 
-		if (userInput < 0) {
-			isPrime = 0;
-		}
-		if (isPrime == 1) {
-			printf("The number %i is a prime number\n", userInput);
-		}
-		else {
-			printf("The number %i is not a prime number\n", userInput);
-		}
-
-		// Counter + 1 to keep track of array size
-		counter++;
-		// End of operations
-		PAUSE;
-		CLS;
-
-		// Get input again to start cycle over
-		printf("Input a number from -50 to 100 or enter -999 to quit the application\n");
-		scanf(" %i", &userInput);
-	}
+	// 
 
 	
 
